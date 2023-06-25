852b4be8fd4948f96c16551fe5f474ae5198c322
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-06-25 13:33:21 -0400

    paginacion en index
    paginacion en index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,34 @@ const { getCategorias } = require('../controllers/getCategoria.js');
 const  getProduct  = require('../controllers/getFakeProd.js')
 const postProd = require('../controllers/postProd1.js')
 const { Router } = require('express');
-
+const productos   = require('../dbdummy.js');
 
 //const {getFakeProd} = require('../dbdummy.js');
+
 const router = Router();
 
 router.get("/categ",  getCategorias);
 
 router.get("/fakeprod", getProduct);
 
-router.post("/postprod",  postProd )
+router.post("/postprod",  postProd );
+
+router.get('/productos', (req, res) => {
+ const page = parseInt(req.query.page) || 1;
+ const perPage = parseInt(req.query.perPage) || 4;
+
+ // Cálculo de los índices para la paginación
+ const startIndex = (page - 1) * perPage;
+ const endIndex = startIndex + perPage;
+
+ // Obtener los productos correspondientes a la página actual
+ const paginatedProductos = productos.slice(startIndex, endIndex);
+
+ res.json(paginatedProductos);
+});
+
+
+
+
 
 module.exports = router ;
